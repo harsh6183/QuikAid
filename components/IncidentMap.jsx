7c14c99f56b112d1a62c709ba074749f3cc1f28a
harsh6183 <harshnimbalkar6181@gmail.com>
2025-07-23 22:11:39 +0530

    Changes in map done

--- components/IncidentMap.jsx
+++ components/IncidentMap.jsx
@@ -1,17 +1,52 @@
-'use client';
+// components/MapPreview.jsx
+"use client";
 import dynamic from "next/dynamic";
+import { useEffect, useState } from "react";
 import "leaflet/dist/leaflet.css";
 
-// Dynamically import MapContainer and other components only on client
-const MapContainer = dynamic(() => import("react-leaflet").then(mod => mod.MapContainer), { ssr: false });
-const TileLayer = dynamic(() => import("react-leaflet").then(mod => mod.TileLayer), { ssr: false });
-const Marker = dynamic(() => import("react-leaflet").then(mod => mod.Marker), { ssr: false });
+// Dynamically import React-Leaflet components
+const MapContainer = dynamic(() => import("react-leaflet").then(m => m.MapContainer), { ssr: false });
+const TileLayer = dynamic(() => import("react-leaflet").then(m => m.TileLayer), { ssr: false });
+const Marker = dynamic(() => import("react-leaflet").then(m => m.Marker), { ssr: false });
 
-export default function IncidentMap({ lat, lng }) {
+export default function MapPreview({ coordinates }) {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+    // Optional: Fix for marker icon not showing
+    import("leaflet").then((L) => {
+      delete L.Icon.Default.prototype._getIconUrl;
+      L.Icon.Default.mergeOptions({
+        iconRetinaUrl:
+          "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
+        iconUrl:
+          "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+        shadowUrl:
+          "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+      });
+    });
+  }, []);
+
+  if (!isClient) return null; // Prevent SSR issues
+
   return (
-    <MapContainer center={[lat, lng]} zoom={16} style={{ height: 300, width: "100%" }}>
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      <Marker position={[lat, lng]} />
-    </MapContainer>
+    coordinates.latitude &&
+    coordinates.longitude && (
+      <div className="rounded-2xl overflow-hidden shadow-lg">
+        <MapContainer
+          center={[coordinates.latitude, coordinates.longitude]}
+          zoom={16}
+          scrollWheelZoom={false}
+          style={{ height: "300px", width: "100%" }}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[coordinates.latitude, coordinates.longitude]} />
+        </MapContainer>
+      </div>
+    )
   );
 }
--- components/report/ReportForm.tsx
+++ components/report/ReportForm.tsx
@@ -5,19 +5,7 @@ import crypto from "crypto";
 import { LocationInput } from "./LocationInput";
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import L from "leaflet";
 
-
-// Fix Leaflet marker icon issue in Next.js/Webpack
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
-  iconUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
-  shadowUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
-});
 
 const REPORT_TYPES = [
   "Theft",

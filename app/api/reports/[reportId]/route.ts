38042206d39ea3d36f1deb7179d9e45b7a9d9a7f
harsh6183 <harshnimbalkar6181@gmail.com>
2025-07-26 21:01:41 +0530

    Changes into files

--- app/api/reports/[reportId]/route.ts
+++ app/api/reports/[reportId]/route.ts
@@ -1,11 +1,11 @@
 import prisma from "@/lib/prisma";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 
 export async function PATCH(
-  request: Request,
-  context: { params: { reportId: string } }  
+  request: NextRequest,
+  { params }: { params: Record<string, string> }
 ) {
   try {
     const session = await getServerSession(authOptions);
@@ -17,7 +17,7 @@ export async function PATCH(
     const { status } = await request.json();
 
     const report = await prisma.report.update({
-      where: { id: context.params.reportId }, 
+      where: { id: params.reportId },
       data: { status },
     });
 

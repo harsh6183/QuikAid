d2217fad2b25a3bf0c7dc0bb725323d14deba8e5
harsh6183 <harshnimbalkar6181@gmail.com>
2025-07-26 20:51:29 +0530

    Changes into the files

--- app/api/reports/[reportId]/route.ts
+++ app/api/reports/[reportId]/route.ts
@@ -1,14 +1,14 @@
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/auth"; 
+import { authOptions } from "@/lib/auth";
 
 export async function PATCH(
   request: Request,
-  { params }: { params: { reportId: string } }
+  context: { params: { reportId: string } }  
 ) {
   try {
-    const session = await getServerSession(authOptions); // 
+    const session = await getServerSession(authOptions);
 
     if (!session) {
       return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
@@ -17,7 +17,7 @@ export async function PATCH(
     const { status } = await request.json();
 
     const report = await prisma.report.update({
-      where: { id: params.reportId },
+      where: { id: context.params.reportId }, 
       data: { status },
     });
 
